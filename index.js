2b1056f91f202451391d44729bcb05ea7508f223
Rudra <merudramanu@gmail.com>
2023-04-25 14:35:52 +0530

    added

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 app.use("/users", userRouter);
 app.use("/flights", authenticate, flightRouter);
