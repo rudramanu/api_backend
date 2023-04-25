653f1496eccaf44b00b301971a69b1bf6c75a89d
Rudra <merudramanu@gmail.com>
2023-04-25 14:32:58 +0530

    added

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { connection } = require("./configs/db");
+const { userRouter } = require("./routers/user.route");
+const { flightRouter } = require("./routers/flight.route");
+const { authenticate } = require("./middleware/authentication");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRouter);
+app.use("/flights", authenticate, flightRouter);
+// app.use("/bookings", authenticate, bookingRouter);
+app.listen(6500, async () => {
+  try {
+    await connection;
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log("Some problem while connecting Database");
+  }
+  console.log("running at 6500");
+});
